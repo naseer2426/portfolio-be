f760cb822eb72f7a793542fa98ef6ca1cb5644d5
naseer2426 <naseerfathima2426@gmail.com>
2025-01-09 23:48:52 +0400

    initialized be

---
+++ src/projects/projects.entity.ts
@@ -0,0 +1,8 @@
+export type Project = {
+    title: string;
+    description: string | null;
+    images?: string[];
+    projectLink: string | null;
+    githubLink: string;
+    tags?: string[];
+}
